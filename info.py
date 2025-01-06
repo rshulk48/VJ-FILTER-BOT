b1be5078e24cc56cd44698c9e6abfae7fcfed635
rshulk48 <167425486+rshulk48@users.noreply.github.com>
2025-01-06 11:45:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -125,10 +125,10 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 
 
 # Shortlink Info
-SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False)) # Set True Or False
-SHORTLINK_URL = environ.get('SHORTLINK_URL', '')
-SHORTLINK_API = environ.get('SHORTLINK_API', '')
-TUTORIAL = environ.get('TUTORIAL', '') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
+SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'speedlinkurl.com')
+SHORTLINK_API = environ.get('SHORTLINK_API', '620c8d2f66f16f02fa8417a269db9703ec95b60a')
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/bdkdjhk/18') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
 
 
 # Others
