24ecd3bc6ff723dacdbb826e8bb11237ec2d10ae
rshulk48 <167425486+rshulk48@users.noreply.github.com>
2025-02-03 08:23:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -88,8 +88,8 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+SDBvltduRtkMGM1')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/hd_moves488')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+SDBvltduRtkyMGM1')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/hd_movies488')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/+SDBvltdutkyMGM1') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/randeep_singh48')
 
